e0f2e0f807c7ebd020569baa197d5b6b963086b6
Matt Groberg <grobergmg@gmail.com>
2019-03-15 10:10:18 -0700

    add js date object to determine age based on birthday and current time

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import './styles.css';
 
 class SpaceAge {
-  constructor(age){
-  this.earth=age;
-  this.mercury=age/0.24;
-  this.venus=age/0.62;
-  this.mars=age/1.88;
-  this.jupiter=age/11.86;
+  constructor(year,month,day,gender,diet,activity){
+  this.year=year;
+  this.month=month;
+  this.day=day;
+  this.gender=gender;
+  this.diet=diet;
+  this.activity=activity;
   }
+
+  calculateYears(){
+  const today= new Date();
+  const birthday= new Date(`${this.year}-${this.month}-${this.day}`);
+  const diff=today-birthday;
+  const years=diff/1000/60/60/24/365.2422;
+  return years.toFixed(2);
+  }
+
+  spaceYears(planet,years){
+    if (planet==="mercury"){
+      return years/0.24;
+    } else if (planet==="venus"){
+      return years/0.62;
+    } else if (planet==="mars"){
+      return years/1.88;
+    } else if (planet==="jupiter"){
+      return years/11.86;
+    }
+  }
+
+  // yearsRemaining(planet){
+  //   this.spaceYears(planet,this.age)
+  // }
 }
+
+const testDemo= new SpaceAge("1988","08","17","male","omnivore","regular");
+
+console.log(testDemo.calculateYears());
