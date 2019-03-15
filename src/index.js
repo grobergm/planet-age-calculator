f53a1653eda75cd5ff5f37c7a6927018b6bca7e5
Matt Groberg <grobergmg@gmail.com>
2019-03-15 09:16:02 -0700

    add class and constructor function with basicage conversions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+
+class SpaceAge {
+  constructor(age){
+  this.earth=age;
+  this.mercury=age/0.24;
+  this.venus=age/0.62;
+  this.mars=age/1.88;
+  this.jupiter=age/11.86;
+  }
+}
